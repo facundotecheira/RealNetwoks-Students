98469ba9281979dd73dd6d7e0eca4382234a842b
facundotecheira <facundotecheira00@gmail.com>
2022-03-22 15:36:24 -0300

    Arrancando con el back-end

--- frontend/src/components/RegisterStudents.js
+++ frontend/src/components/RegisterStudents.js
@@ -1,9 +1,30 @@
 import Nav from "./Nav"
+import { useRef,useEffect,useState } from "react"
+import toasty from "./Toast"
+
 const RegisterStudents = () =>{
+
+    const name = useRef()
+    const email = useRef()
+    const studentNumber = useRef()
+    const phoneNumber = useRef()
 
     const HandleSubmit = (e) =>{
         e.preventDefault()
-        console.log('hola hola')
+        if(name.current.value == "" || email.current.value == "" || studentNumber.current.value == "" || phoneNumber.current.value == ""){
+            return toasty('error','The fields cannot be empty')
+        }
+
+        const data = {
+
+            name: name.current.value,
+            email: email.current.value,
+            studentNumber: studentNumber.current.value,
+            phoneNumber: phoneNumber.current.value
+        }
+
+        
+        
     }
 
     return(
@@ -12,10 +33,10 @@ const RegisterStudents = () =>{
             <h1 className="titleForm">Register a new student</h1>
             <div className="containerFormNotas" onSubmit={HandleSubmit}>
             <form className="form">
-                <input type= "text" placeholder="Enter you Name"></input>
-                <input type="email" placeholder="Enter you Email"></input>
-                <input type= "text" placeholder="Enter you Student number"></input>
-                <input type= "text" placeholder="Enter you phone number"></input>
+                <input ref={name} type= "text" placeholder="Enter you Name"></input>
+                <input ref={email} type="email" placeholder="Enter you Email"></input>
+                <input ref={studentNumber} type= "text" placeholder="Enter you Student number"></input>
+                <input ref={phoneNumber} type= "text" placeholder="Enter you phone number"></input>
                 <input type="submit" className="formSubmit" value="Register student"></input>
             </form>
             </div>
