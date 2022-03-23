5a16d6070e24b05132133b6e7dd20ff73689a23e
facundotecheira <facundotecheira00@gmail.com>
2022-03-23 17:52:28 -0300

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,7 +11,7 @@ function App() {
    <BrowserRouter>
    
    <Routes>
-
+     
     <Route path='/' element={<Home/>}/>
     <Route path='/register' element={<RegisterStudents/>}/>
     <Route path='/list' element={<ListStudents/>}/>
