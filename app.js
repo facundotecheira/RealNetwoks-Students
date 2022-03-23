183f120388c5bca73fe0080436f36b0e1bb0b4f7
facundotecheira <facundotecheira00@gmail.com>
2022-03-23 18:45:05 -0300

    .

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const express = require('express'),
-      morgan = require('morgan'),
-      Router = require('./routes/routes')
-      cors = require('cors')
+   morgan = require('morgan'),
+   Router = require('./routes/routes')
+cors = require('cors')
+path = require('path')
 
 require('./config/database')
 
@@ -10,14 +11,21 @@ const app = express()
 
 // Settings
 
-app.set('port',process.env.PORT || 4000)
+app.set('port', process.env.PORT || 4000)
 
 // Middleware
 
 app.use(morgan('dev'))
    .use(cors())
    .use(express.json())
-   .use('/api',Router)
+   .use('/api', Router)
 
+
+if (process.env.NODE_ENV === 'production') {
+   app.use(express.static("client/build"))
+   app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname + "/client/build/index.html"))
+   })
+}
 
 module.exports = app
